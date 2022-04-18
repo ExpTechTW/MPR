ab409a88c8f8e9fdb292ce8f308de3d6bcac953b
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-04-18 21:03:43 +0800

    Add files via upload

---
+++ Firmware/config.js
@@ -0,0 +1,20 @@
+const PreRelease=false
+const Python=false
+
+const Token = ""
+const API_POST = "http://150.117.110.118:10150"
+const API_WebSocket = "ws://150.117.110.118:910"
+const APIkey = ""
+
+async function main() {
+
+}
+
+module.exports = {
+    Token,
+    API_POST,
+    API_WebSocket,
+    APIkey,
+    PreRelease,
+    Python
+}
