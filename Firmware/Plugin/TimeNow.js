29cc58650e599a7ff03c2685dfe5746bc7128e91
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-04-18 23:24:12 +0800

    Add files via upload

---
+++ Firmware/Plugin/TimeNow.js
@@ -0,0 +1,28 @@
+const Info = {
+    "version": "1.0.0",
+    "name": "TimeNow",
+    "author": "whes1015"
+}
+
+const Event = [
+    "messageCreate"
+]
+
+const pluginLoader = require('../Core/pluginLoader')
+
+async function messageCreate(client, message) {
+    let now = new Date()
+    let Now = now.getFullYear() +
+        "/" + (now.getMonth() + 1) +
+        "/" + now.getDate() +
+        " " + now.getHours() +
+        ":" + now.getMinutes() +
+        ":" + now.getSeconds()
+    message.reply(await pluginLoader.embed(Now, null, Info.author, "https://raw.githubusercontent.com/ExpTechTW/API/%E4%B8%BB%E8%A6%81%E7%9A%84-(main)/image/Icon/ExpTech.png"))
+}
+
+module.exports = {
+    Info,
+    Event,
+    messageCreate
+}
